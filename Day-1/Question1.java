28722e3875dcb60ef2beb3a74b1818603899fca5
Akshat Mishra <akshatmishra4540@gmail.com>
2026-06-01 12:02:06 +0530

    Update Question1.java

--- Day-1/Question1.java
+++ Day-1/Question1.java
@@ -5,7 +5,7 @@ public class Question1 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int sum = 0;
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){ // Loop to calculate the sum of numbers from 1 to n
             sum = sum + i;
         }
         System.out.println("The sum of numbers from 1 to " + n + " is: " + sum);
