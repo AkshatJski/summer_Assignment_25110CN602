7c76ebfe58e8bb70f7c89547622028677e7e87ac
Akshat Mishra <akshatmishra4540@gmail.com>
2026-06-03 12:50:56 +0530

    Day-3 uploaded.

--- Day-2/Question5.java
+++ Day-2/Question5.java
@@ -5,12 +5,12 @@ public static void main(String args[]){
     System.out.println("Enter the number to find the sum of its digits:");
     int num=in.nextInt();
     int sum=0;
-    while(num!=0){
+    while(num!=0){ // Loop to extract each digit and add to sum
         int r=num%10;
         sum=sum+r;
         num=num/10;
     }
     System.out.println("Sum of digits: " + sum);
-    in.close();
+    in.close(); // Closing the scanner to prevent memory leak
 }
 }
